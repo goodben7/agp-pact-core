9e1f3e298159b49bb7ef6c41a63b81f8a2a4e9e5
Paul Nganda <paulngandasmith@gmail.com>
2025-06-17 18:44:19 +0100

    generation migration for complaintConsequences

---
+++ migrations/Version20250617174334.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250617174334 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE complaint_consequence CHANGE severity_id severity_id VARCHAR(16) DEFAULT NULL, CHANGE impact_description impact_description LONGTEXT DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE complaint_consequence CHANGE severity_id severity_id VARCHAR(16) NOT NULL, CHANGE impact_description impact_description LONGTEXT NOT NULL
+        SQL);
+    }
+}
---
+++ src/Constant/GeneralParameterAssignmentType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constant;
+
+class GeneralParameterAssignmentType
+{
+    public const CATEGORY_ASSIGNMENT_TYPE = 'AssignmentType';
+
+    public const ASSIGN_TO_USER_CODE = 'ASSIGN_TO_USER';
+    public const ASSIGN_TO_USER_VALUE = 'Assigner à un Utilisateur';
+
+    public const ASSIGN_TO_COMPANY_CODE = 'ASSIGN_TO_COMPANY';
+    public const ASSIGN_TO_COMPANY_VALUE = 'Assigner à une Entreprise';
+}
