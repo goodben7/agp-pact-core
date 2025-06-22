cbcb45a06cb1de70da6770fe21bbd06f199e2c3d
Paul Nganda <paulngandasmith@gmail.com>
2025-06-22 15:52:58 +0100

    add available actions

---
+++ src/Provider/Complaint/ComplaintProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Provider\Complaint;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Entity\Complaint;
+use App\Entity\WorkflowTransition;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+readonly class ComplaintProvider implements ProviderInterface
+{
+    public function __construct(
+        #[Autowire(service: 'api_platform.doctrine.orm.state.item_provider')]
+        private ProviderInterface $itemProvider,
+        private EntityManagerInterface $em
+    ) {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        $result = $this->itemProvider->provide($operation, $uriVariables, $context);
+
+        if ($result instanceof Complaint) {
+            /** @var Complaint $complaint */
+            $complaint = $result;
+
+            $transitions = $this->em->getRepository(WorkflowTransition::class)->findBy(['fromStep' => $complaint->getCurrentWorkflowStep()]);
+
+            foreach ($transitions as $transition) {
+                $complaint->addAvailableAction($transition->getAction());
+            }
+            return $complaint;
+        }
+
+        return $result;
+    }
+}
