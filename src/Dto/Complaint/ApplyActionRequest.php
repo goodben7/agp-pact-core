44dbb9e94c4e41aa5f4909c48ee7a71d43a2de4e
Paul Nganda <paulngandasmith@gmail.com>
2025-06-22 18:12:04 +0100

    update complaint apply action input formats

--- src/Dto/Complaint/ApplyActionRequest.php
+++ src/Dto/Complaint/ApplyActionRequest.php
@@ -2,16 +2,56 @@
 
 namespace App\Dto\Complaint;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ApplyActionRequest
 {
     #[Assert\NotBlank]
     public ?string $actionId = null;
+
     public array $dynamicFields = [];
+
+    public function __construct()
+    {
+        $this->dynamicFields = [];
+    }
+
+    public function setFromArray(array $data): self
+    {
+        if (isset($data['actionId'])) {
+            $this->actionId = $data['actionId'];
+            unset($data['actionId']);
+        }
+
+        $this->dynamicFields = $data;
+
+        return $this;
+    }
 
     public function toArray(): array
     {
         return $this->dynamicFields;
+    }
+
+    public function hasFile(): bool
+    {
+        foreach ($this->dynamicFields as $value) {
+            if ($value instanceof UploadedFile) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getFiles(): array
+    {
+        $files = [];
+        foreach ($this->dynamicFields as $key => $value) {
+            if ($value instanceof UploadedFile) {
+                $files[$key] = $value;
+            }
+        }
+        return $files;
     }
 }
