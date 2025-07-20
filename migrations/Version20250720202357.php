a1a8306571bb8cd0c8538f526e881afe83d15e09
Paul Nganda <paulngandasmith@gmail.com>
2025-07-20 21:24:45 +0100

    fix migrations error

--- migrations/Version20250720202357.php
+++ migrations/Version20250720202357.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250720195942 extends AbstractMigration
+final class Version20250720202357 extends AbstractMigration
 {
     public function getDescription(): string
     {
