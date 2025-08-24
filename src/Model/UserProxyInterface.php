cdfa684621bc21d7f96aa7de861fa0e93d0442ad
bmukena85 <bmukena85@gmail.com>
2025-08-24 19:22:18 +0100

    feat(user): update User entity and UserProxyInterface

--- src/Model/UserProxyInterface.php
+++ src/Model/UserProxyInterface.php
@@ -12,8 +12,9 @@ interface UserProxyInterface
     public const PERSON_COMPANY = 'ENT';
     public const PERSON_CONTROL_MISSION = 'CTL';
     public const PERSON_INFRASTRUCTURE_CELL = 'INF';
+    public const PERSON_LAMBDA = 'LAM';
+    public const PERSON_MANAGER = 'MGR';
     public const PERSON_WORLD_BANK = 'WBK';
     public const PERSON_COMPLAINANT = 'CMP';
-    public const PERSON_LAMBDA = 'LAM';
     public const PERSON_ADMINISTRATOR_MANAGER = 'ADM_MGR';
 }
